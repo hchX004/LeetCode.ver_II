e081f8fade80ead80522b2367378e1b37de73b39
hchX004 <hecenhao@outlook.com>
2019-01-21 22:31:27 +0800

    #28_20180121_Java

---
+++ Java/28. Implement strStr()/20190121Solution.java
@@ -0,0 +1,15 @@
+//my answer
+
+class Solution {
+    public int strStr(String haystack, String needle){
+        int l1 = haystack.length(), l2 = needle.length();
+        if(l1 < l2) return -1;
+        else if(l2 == 0) return 0;
+        for(int i = 0; i <= l1-l2; i++) {
+            if(haystack.substring(i,i + l2).equals(needle)) return i;
+        }
+        return -1;
+    }
+}
+
+//其实可以用这个复习一下KMP……
