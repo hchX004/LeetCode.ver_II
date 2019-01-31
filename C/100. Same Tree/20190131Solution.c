e9d870d81ea653ba3e40ab15f725546f8ad88cef
hchX004 <hecenhao@outlook.com>
2019-01-31 22:19:39 +0800

    #100_20180131_C

---
+++ C/100. Same Tree/20190131Solution.c
@@ -0,0 +1,32 @@
+//my answer
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p == NULL && q == NULL) return 1;
+    else if(p == NULL || q == NULL) return 0;
+    else if(p->val == q->val){
+        if(isSameTree(p->left, q->left) && isSameTree(p->right, q->right)) return 1;
+        else return 0;
+    }
+    else return 0;
+}
+
+//best answer
+
+/*
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p&&q)
+        return(p->val==q->val)&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+    else if(p==NULL&&q==NULL)
+        return true;
+    else 
+        return false;
+}
+*/
