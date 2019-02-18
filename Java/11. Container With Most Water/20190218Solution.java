07d085068f0709126358b68b334c59ab9e59b19e
hchX004 <hecenhao@outlook.com>
2019-02-18 22:44:44 +0800

    #11_20190218_Java

---
+++ Java/11. Container With Most Water/20190218Solution.java
@@ -0,0 +1,47 @@
+//my answer excellent
+
+class 20190218Solution {
+    public int maxArea(int[] height) {
+        int len = height.length, result = 0, tempMax;
+        for(int i = 0, j = len-1; i < len && j >= i;){
+            if(height[i] < height[j]){
+                if((tempMax = height[i]*(j-i)) >= result) result = tempMax;
+                i++;
+            }
+            else{
+                if((tempMax = height[j]*(j-i)) >= result) result = tempMax;
+                j--;
+            }
+        }
+        return result;
+    }
+}
+
+/*
+    方法：双指针法
+*/
+
+//best answer
+
+/*
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0, nmax = 0;
+        int i = 0, j = height.length - 1;
+        int a;
+        while(i < j){
+            if (height[i] <= height[j]){
+                nmax =  height[i] * (j - i);
+                a = height[i];
+                while(height[++i] <= a && i < j);
+            }else{
+                nmax =  height[j] * (j - i);
+                a = height[j];
+                while(height[--j] <= a && i < j);
+            }
+            if (nmax > max)max = nmax;
+        }
+        return max;
+    }
+}
+*/
