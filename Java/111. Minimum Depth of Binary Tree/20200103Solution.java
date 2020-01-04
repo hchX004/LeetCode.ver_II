c3622733f2feb1ee001ba7ff5b7576e8c983caf2
hchX004 <hecenhao@outlook.com>
2020-01-04 23:29:41 +0800

    add a solution

--- Java/111. Minimum Depth of Binary Tree/20200103Solution.java
+++ Java/111. Minimum Depth of Binary Tree/20200103Solution.java
@@ -17,3 +17,41 @@ class 20200103Solution {
         return root.left == null || root.right == null ? ml+mr+1 : Math.min(ml, mr)+1;
     }
 }
+
+
+/*
+
+
+
+import javafx.util.Pair;
+class Solution {
+  public int minDepth(TreeNode root) {
+    LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+    if (root == null) {
+      return 0;
+    }
+    else {
+      stack.add(new Pair(root, 1));
+    }
+
+    int current_depth = 0;
+    while (!stack.isEmpty()) {
+      Pair<TreeNode, Integer> current = stack.poll();
+      root = current.getKey();
+      current_depth = current.getValue();
+      if ((root.left == null) && (root.right == null)) {
+        break;
+      }
+      if (root.left != null) {
+        stack.add(new Pair(root.left, current_depth + 1));
+      }
+      if (root.right != null) {
+        stack.add(new Pair(root.right, current_depth + 1));
+      }
+    }
+    return current_depth;
+  }
+}
+
+
+ */
