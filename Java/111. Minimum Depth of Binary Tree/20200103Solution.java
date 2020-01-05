fb862cfacd3734d4f523c431d0453db8311178d0
hchX004 <hecenhao@outlook.com>
2020-01-05 23:34:01 +0800

    add som learning about 111. Minimum Depth of Binary Tree

--- Java/111. Minimum Depth of Binary Tree/20200103Solution.java
+++ Java/111. Minimum Depth of Binary Tree/20200103Solution.java
@@ -1,4 +1,4 @@
-//my answer
+﻿//my answer
 
 
 
@@ -18,25 +18,59 @@ class 20200103Solution {
     }
 }
 
-
 /*
-
-
+深度优先搜索迭代
 
 import javafx.util.Pair;
 class Solution {
   public int minDepth(TreeNode root) {
-    LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+    LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>(); //初始化栈，LIFO(后进先出)栈
     if (root == null) {
-      return 0;
+      return 0;                                                     //空则返回0
     }
     else {
-      stack.add(new Pair(root, 1));
+      stack.add(new Pair(root, 1));                                 //添加第一层
     }
 
-    int current_depth = 0;
+    int min_depth = Integer.MAX_VALUE;                              //设置深度为最大整数
+    while (!stack.isEmpty()) {                                      //如果栈不为空
+      Pair<TreeNode, Integer> current = stack.pollLast();           //弹出栈顶第一对
+      root = current.getKey();
+      int current_depth = current.getValue();                       //得到根和层数
+      if ((root.left == null) && (root.right == null)) {            //如果左右子树都为空
+        min_depth = Math.min(min_depth, current_depth);             //更新最小深度
+      }
+      if (root.left != null) {                                      //如果左子树不为空
+        stack.add(new Pair(root.left, current_depth + 1));          //把左子树  加入栈中，层数+1
+      }
+      if (root.right != null) {                                     //如果右子树为空
+        stack.add(new Pair(root.right, current_depth + 1));         //把右子树加入栈中，层数+1
+      }
+    }
+    return min_depth;                                               //返回所得的最小层数
+  }
+}
+ */
+
+
+
+/*
+广度优先搜索迭代
+
+import javafx.util.Pair;
+class Solution {
+  public int minDepth(TreeNode root) {
+    LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>(); //初始化队列，FIFO(先进先出)队列
+    if (root == null) {
+      return 0;                                                     //空则返回0
+    }
+    else {
+      stack.add(new Pair(root, 1));                                 //添加第一层
+    }
+
+    int current_depth = 0;                                          //初始化当前层数
     while (!stack.isEmpty()) {
-      Pair<TreeNode, Integer> current = stack.poll();
+      Pair<TreeNode, Integer> current = stack.poll();               //弹出队列第一对
       root = current.getKey();
       current_depth = current.getValue();
       if ((root.left == null) && (root.right == null)) {
@@ -49,9 +83,7 @@ class Solution {
         stack.add(new Pair(root.right, current_depth + 1));
       }
     }
-    return current_depth;
+    return current_depth;                                           //最先找到为空的叶的层数为最小的层数
   }
 }
-
-
  */
