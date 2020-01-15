1d0d1b9546d679e3e4c53167f46931e752aababf
hchX004 <hecenhao@outlook.com>
2020-01-15 20:12:58 +0800

    commit  122. Best Time to Buy and Sell Stock II 2020115

---
+++ Java/122. Best Time to Buy and Sell Stock II/20200115Solution.java
@@ -0,0 +1,14 @@
+//my answer
+
+
+//只要后面一天的价格比前一天高就购买，贪心算法
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, differ;
+        for(int i = 0; i < prices.length-1; i++){
+            differ = prices[i+1] - prices[i];
+            if(differ > 0) profit += differ;
+        }
+        return profit;
+    }
+}
