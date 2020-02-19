b8e53bfe80d3d9056a6d19d0b8d6646b2b26d627
hchX004 <hecenhao@outlook.com>
2020-02-19 20:31:33 +0800

    add some methods in 78.Subsets

--- Java/78. Subsets/20200218Solution.java
+++ Java/78. Subsets/20200218Solution.java
@@ -20,4 +20,28 @@ class 20200218Solution {
 
 /*
 
+//位运算
+    public static List<List<Integer>> binaryBit(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int i = 0; i < (1 << nums.length); i++) {
+            List<Integer> sub = new ArrayList<Integer>();
+            for (int j = 0; j < nums.length; j++)
+                if (((i >> j) & 1) == 1) sub.add(nums[j]);
+            res.add(sub);
+        }
+        return res;
+    }
+
+//递归枚举
+    public static void recursion(int[] nums, int i, List<List<Integer>> res) {
+        if (i >= nums.length) return;
+        int size = res.size();
+        for (int j = 0; j < size; j++) {
+            List<Integer> newSub = new ArrayList<Integer>(res.get(j));
+            newSub.add(nums[i]);
+            res.add(newSub);
+        }
+        recursion(nums, i + 1, res);
+    }
+
  */
