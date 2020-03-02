d323d77abc2d6021fedad126c7774a199826e5d3
hchX004 <hecenhao@outlook.com>
2020-03-02 22:09:45 +0800

    commit 950. Reveal Cards In Increasing Order in 2020.03.02

---
+++ Java/950. Reveal Cards In Increasing Order/20200302Solution.java
@@ -0,0 +1,16 @@
+
+
+
+class 20200302Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int[] ans = new int[deck.length];
+        int flag = 1;
+        Arrays.sort(deck);
+        for(int i = 0, p = 0; i < deck.length; i++){
+            while(flag < 2) flag += ans[(p++) % deck.length] == 0 ? 1 : 0;
+            ans[(p - 1) % deck.length] = deck[i];
+            flag = 0;
+        }
+        return ans;
+    }
+}
