13e132d5a30a74610eb8a7603841d89e450120ce
hchX004 <hecenhao@outlook.com>
2020-03-18 20:08:17 +0800

    commit 52. N-Queens II in 2020.03.18

--- Java/52. N-Queens II/20200317Solution.java
+++ Java/52. N-Queens II/20200317Solution.java
@@ -2,47 +2,43 @@
 
 
 class 20200317Solution {
-  public boolean is_not_under_attack(int row, int col, int n,
-                                     int [] rows,
-                                     int [] hills,
-                                     int [] dales) {
-    int res = rows[col] + hills[row - col + 2 * n] + dales[row + col];
-    return (res == 0) ? true : false;
-  }
 
-  public int backtrack(int row, int count, int n,
-                       int [] rows,
-                       int [] hills,
-                       int [] dales) {
-    for (int col = 0; col < n; col++) {
-      if (is_not_under_attack(row, col, n, rows, hills, dales)) {
-        // place_queen
-        rows[col] = 1;
-        hills[row - col + 2 * n] = 1;  // "hill" diagonals
-        dales[row + col] = 1;   //"dale" diagonals    
+	public int totalNQueens(int n) {
+		int rows[] = new int[n];
+		// 右斜对角线，2n-1层
+		int hills[] = new int[2 * n - 1];
+		// 左斜对角线，2n-1层
+		int dales[] = new int[2 * n - 1];
 
-        // if n queens are already placed
-        if (row + 1 == n) count++;
-        // if not proceed to place the rest
-        else count = backtrack(row + 1, count, n,
-                rows, hills, dales);
+		return backTrack(0, 0, n, rows, hills, dales);
+	}
 
-        // remove queen
-        rows[col] = 0;
-        hills[row - col + 2 * n] = 0;
-        dales[row + col] = 0;
-      }
-    }
-    return count;
-  }
 
-  public int totalNQueens(int n) {
-    int rows[] = new int[n];
-    // "hill" diagonals
-    int hills[] = new int[4 * n - 1];
-    // "dale" diagonals
-    int dales[] = new int[2 * n - 1];
 
-    return backtrack(0, 0, n, rows, hills, dales);
-  }
+	public int backTrack(int row, int count, int n, int[] rows, int[] hills, int[] dales) {
+		for (int col = 0; col < n; col++) {
+			if (isNotUnderAttack(row, col, n, rows, hills, dales)) {
+        		// 放置皇后
+        		rows[col] = 1;
+        		hills[row - col + n - 1] = 1;
+        		dales[row + col] = 1;
+
+        		// 如果已经有n个皇后可以放置
+        		if (row + 1 == n) count++;
+        		// 如果没有则回溯
+        		else count = backTrack(row + 1, count, n, rows, hills, dales);
+
+        		// 移走皇后
+        		rows[col] = 0;
+        		hills[row - col + n - 1] = 0;
+        		dales[row + col] = 0;
+			}
+		}
+		return count;
+	}
+
+	public boolean isNotUnderAttack(int row, int col, int n, int[] rows, int[] hills, int[] dales) {
+		int res = rows[col] + hills[row - col + n - 1] + dales[row + col];
+		return (res == 0) ? true : false;
+	}
 }
