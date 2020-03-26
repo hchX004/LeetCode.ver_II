863b8be6072b60ff594ce6e1a745c51d3c7265c7
hchX004 <hecenhao@outlook.com>
2020-03-26 21:31:44 +0800

    commit 225. Implement Stack using Queues in 2020.03.26

--- Java/225. Implement Stack using Queues/20200325Solution.java
+++ Java/225. Implement Stack using Queues/20200325Solution.java
@@ -1,3 +1,45 @@
 
 
 
+class MyStack {
+
+    private List<Integer> list;
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        list = new ArrayList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        list.add(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        int size = list.size();
+        int popElement = list.get(size - 1);
+        list.remove(size - 1);
+        return popElement;
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return list.get(list.size() - 1);
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        //System.out.println(list.size());
+        return list.size() <= 0 ? true : false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
