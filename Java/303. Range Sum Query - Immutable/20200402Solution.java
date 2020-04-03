111f93b176c7421b1449813fa5efee4247bfd39e
hchX004 <hecenhao@outlook.com>
2020-04-03 08:57:54 +0800

    commit 303. Range Sum Query - Immutable in 2020.04.03

--- Java/303. Range Sum Query - Immutable/20200402Solution.java
+++ Java/303. Range Sum Query - Immutable/20200402Solution.java
@@ -3,19 +3,22 @@
 
 class NumArray {
 
-    private int[] sums;
+    private int[] sums;		//算出和数组
 
     public NumArray(int[] nums) {
-        sums = new int[nums.length];
-        if (nums.length == 0) return;
-        sums[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sums[i] += sums[i - 1] + nums[i];
+        sums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
         }
     }
     
     public int sumRange(int i, int j) {
-        if (i == 0) return sums[j];
-        else return sums[j] - sums[i - 1];
+        return sums[j + 1] - sums[i];
     }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
